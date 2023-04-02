52cafd64e123b23ddedaa8bbf21ce0546ab8c50a
huzaifa <huzaifaayuba@gmail.com>
2023-04-02 12:44:05 -0700

    git add .

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+given URL as parameter, fetch URL and display value from response header
+usage: ./5-hbtn_header https://intranet.hbtn.io
+"""
+import requests
+from sys import argv
